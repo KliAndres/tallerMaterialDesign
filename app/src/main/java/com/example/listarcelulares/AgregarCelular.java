4b021e57d730a5930b8851923359cdfbb862f039
Carlos Pacheco <cali.pacheco0826@gmail.com>
2020-06-17 17:07:15 -0500

    Validacion de vacios listo

--- app/src/main/java/com/example/listarcelulares/AgregarCelular.java
+++ app/src/main/java/com/example/listarcelulares/AgregarCelular.java
@@ -1,9 +1,11 @@
 package com.example.listarcelulares;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -50,29 +52,70 @@ public class AgregarCelular extends AppCompatActivity {
         storageReference = FirebaseStorage.getInstance().getReference();
     }
 
-    public void guardar(View v){
-        String mar, mod, ime,mem,ra, id;
+    public void guardar(View v) {
+        String mar, mod, ime, mem, ra, id;
         int fot;
         Celular celular;
+        InputMethodManager imp = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        InputMethodManager imp= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (validar()) {
+            mar = marca.getText().toString();
+            mod = modelo.getText().toString();
+            ime = imei.getText().toString();
+            mem = memoria.getText().toString();
+            ra = ram.getText().toString();
+            fot = foto_aleatoria();
+            id = Datos.getId();
 
-        mar = marca.getText().toString();
-        mod = modelo.getText().toString();
-        ime = imei.getText().toString();
-        mem = memoria.getText().toString();
-        ra = ram.getText().toString();
-        fot = foto_aleatoria();
-        id = Datos.getId();
+            celular = new Celular(mar, mod, ime, mem, ra, fot, id);
+            celular.guardar();
+            subir_foto(id, fot);
+            limpiar();
+            imp.hideSoftInputFromWindow(imei.getWindowToken(), 0);
 
-        celular = new Celular(mar, mod, ime, mem, ra, fot, id);
-        celular.guardar();
-        subir_foto(id, fot);
-        limpiar();
-        imp.hideSoftInputFromWindow(imei.getWindowToken(), 0);
-
-        Snackbar.make(v, getString(R.string.mensajeGuardarC),Snackbar.LENGTH_LONG).show();
+            Snackbar.make(v, getString(R.string.mensajeGuardarC), Snackbar.LENGTH_LONG).show();
+        }
     }
+
+    public boolean validar(){
+        String e_marca, e_modelo, e_imei, e_memoria, e_ram, e_imei_digitos;
+
+        e_marca="Escriba la marca";
+        e_modelo="Esciba el modelo";
+        e_imei="Escriba los 15 digitos del IMEI";
+        e_memoria="Escriba el tamaño de memoria";
+        e_ram ="Escriba el tamaño de RAM";
+        e_imei_digitos="Deben ser 15 caracteres";
+
+        if (marca.getText().toString().isEmpty()){
+            marca.setError(e_marca);
+            marca.requestFocus();
+            return false;
+        }else if (modelo.getText().toString().isEmpty()){
+            modelo.setError(e_modelo);
+            modelo.requestFocus();
+            return false;
+        }else if (imei.getText().toString().isEmpty()){
+            imei.setError(e_imei);
+            imei.requestFocus();
+            return false;
+        }else if (memoria.getText().toString().isEmpty()){
+            memoria.setError(e_memoria);
+            memoria.requestFocus();
+            return false;
+        }else if (ram.getText().toString().isEmpty()){
+            ram.setError(e_ram);
+            ram.requestFocus();
+            return false;
+        }else if (imei.length()!=15){
+            imei.setError(e_imei_digitos);
+            imei.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
+
     public void subir_foto(String id, int foto){
         StorageReference child = storageReference.child(id);
         Uri uri = Uri.parse("android.resource://"+R.class.getPackage().getName()+"/"+foto);
