b0278611003570554b095abe4b909127accc96ad
Carlos Pacheco <cali.pacheco0826@gmail.com>
2020-06-16 19:18:37 -0500

    Adaptador Celular parcialmente completo

--- app/src/main/java/com/example/listarcelulares/Adaptador_Celular.java
+++ app/src/main/java/com/example/listarcelulares/Adaptador_Celular.java
@@ -1,5 +1,6 @@
 package com.example.listarcelulares;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -20,17 +21,22 @@ public class Adaptador_Celular extends RecyclerView.Adapter<Adaptador_Celular.Ce
     @NonNull
     @Override
     public CelularViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_celular, parent,false);
+        return new CelularViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CelularViewHolder holder, int position) {
-
+        Celular c = celulares.get(position);
+        holder.foto.setImageResource(c.getFoto());
+        holder.marca.setText(c.getMarca());
+        holder.modelo.setText(c.getModelo());
+        holder.imei.setText(c.getImei());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return celulares.size();
     }
 
     public static class CelularViewHolder extends RecyclerView.ViewHolder{
